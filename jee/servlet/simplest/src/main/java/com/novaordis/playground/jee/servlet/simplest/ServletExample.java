115bac16041f722e15128bdbb338e5cf83f62cf1
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-01-09 13:27:17 -0800

    working jee.servlet.simplest example

---
+++ jee/servlet/simplest/src/main/java/com/novaordis/playground/jee/servlet/simplest/ServletExample.java
@@ -0,0 +1,81 @@
+package com.novaordis.playground.jee.servlet.simplest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Ovidiu Feodorov <ovidiu@novaordis.com>
+ * @since 1/9/16
+ */
+public class ServletExample extends HttpServlet {
+
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    private static final Logger log = LoggerFactory.getLogger(ServletExample.class);
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    // HttpServlet overrides -------------------------------------------------------------------------------------------
+
+    @Override
+    public void init()
+    {
+        log.info(this + " initialized");
+    }
+
+    @Override
+    public void destroy()
+    {
+        log.info(this + " destroyed");
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+    {
+        log.info(this + " handling GET");
+
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+
+        out.println("<html>");
+        out.println("GET OK");
+        out.println("</html>");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res)  throws ServletException, IOException {
+
+        log.info(this + " handling POST");
+
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+
+        out.println("<html>");
+        out.println("POST OK");
+        out.println("</html>");
+    }
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+
+}
