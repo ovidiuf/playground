12990623acbb4f21b8b6199dd4db3170dd2a10a8
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-01-19 00:33:45 -0800

    added a noop jboss-deployment-structure.xml stub

--- jee/servlet/simplest/src/main/java/com/novaordis/playground/jee/servlet/simplest/ServletExample.java
+++ jee/servlet/simplest/src/main/java/com/novaordis/playground/jee/servlet/simplest/ServletExample.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author Ovidiu Feodorov <ovidiu@novaordis.com>
