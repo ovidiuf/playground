48d98566c062ede7f6cdc1b66eedeb71f7648d1a
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-03-18 15:09:08 -0700

    JEE EAR with Maven

--- jee/ear/war-and-lib/README.md
+++ jee/ear/war-and-lib/README.md
@@ -4,4 +4,7 @@ An example of how to build a simple JEE EAR with an embedded lib and WAR.
 The functionality is not relevant, but the Maven configuration that builds
 it is.
 
+Knowledge Base:
+
+* https://kb.novaordis.com/index.php/Maven_ear_Plugin
 
---
+++ jee/ear/war-and-lib/lib/src/main/java/io/novaordis/playground/jee/ear/warandlib/lib/LibraryFunctionality.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Nova Ordis LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.novaordis.playground.jee.ear.warandlib.lib;
+
+/**
+ * @author Ovidiu Feodorov <ovidiu@novaordis.com>
+ * @since 3/18/16
+ */
+public class LibraryFunctionality {
+
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    public static String getValue() {
+        return "...";
+    }
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+
+}
