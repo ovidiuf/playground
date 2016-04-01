4edcc5e5b769b9e67fdf6563874d695e37323d39
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-04-01 16:55:35 -0700

    command line arguments

---
+++ go/command-line-args/command-line-arg-parser.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+)
+
+
+func main() {
+
+    //
+    // Define the flags
+    //
+
+    sPtr := flag.String("s", ".", "the path vale")
+    iPtr := flag.Int("i", 1, "the i value")
+
+    flag.Parse()
+
+    fmt.Println("flag s value: ", *sPtr)
+    fmt.Println("flag i value: ", *iPtr)
+
+    //
+    // The rest of the arguments
+    //
+
+    args := flag.Args()
+
+    for j, arg := range args {
+        fmt.Println(j, arg)
+    }
+
+}
