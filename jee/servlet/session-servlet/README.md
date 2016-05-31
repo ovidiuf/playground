d1a46bd0064db98ef0769d582c9c6233b9b080f7
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-05-31 18:16:56 -0400

    first working version: session-servlet

--- jee/servlet/session-servlet/README.md
+++ jee/servlet/session-servlet/README.md
@@ -4,4 +4,72 @@
 
 A simple JEE servlet that can be deployed within a JEE container and used to test continuity, load balancing, failover, session stickiness, etc. It has been tested to work with WildFly/EAP and with Tomcat.
 
+## Build
+
+    mvn clean package
+    
+## Deploy
+    
+Copy ./target/session-servlet.war into the deployment directory of the application server.
+    
+## Authentication
+    
+The default build produces a servlet that *does NOT require authentication*.
+    
+If you want authentication, do this (JBoss 5 procedure, may need to be updated for WildFly):
+    
+1. Un-comment web.xml section starting with &lt;security-constraint&gt; and ending with &lt;/security-role&gt;
+    
+2. Replace "admin" with a valid role. The replacement must be done in both places where &lt;role-name&gt; is mentioned. For example, if deployed on JBoss 5, pick up an appropriate role from $JBOSS_HOME/server/$JBOSS_PROFILE/conf/props/jmx-console-roles.properties.
+    
+3. Enable &lt;security-domain&gt; in jboss-web.xml and make sure it points to the correct one on the server.
+    
+## HTTP Session
+
+The smoke servlet will NOT establish a HTTP session by default.
+
+If you wish it to establish a session, call it with "establish-session" parameter on the first request.
+
+Example: http://locahost:8080/session-servlet?establish-session
+
+After the first request, obviously there's no need for "establish-session" anymore, the browser/server ensemble maintain the one that was established. The current implementation will throw an exception if it sees "establish-session=true" again.
+
+##Root Context
+
+You can change the root context as follows:
+
+###On JBoss
+
+####Method One
+
+Simply deploy the WAR under the desired name.
+
+####Method Two
+
+TODO: use jboss-web.xml and root-context.
+
+###On Tomcat
+
+Simply deploy the WAR under the desired name.
+
+##Test Plan
+
+###1. Simple Availability
+
+Build and deploy. 
+
+Go to http://&lt;server-address&gt;:&lt;server-port&gt;/session-servlet
+
+It will return a simple HTTP page listing relevant information regarding the execution.
+
+###2. Session Experiments
+
+Establish a session with http://&lt;server-address&gt;:&lt;server-port&gt;/session-servlet?establish-session
+
+Then drop the parameter, the browser/server should maintain the session until it expires.
+
+
+    
+
+
 
---
+++ jee/servlet/session-servlet/src/main/java/io/novaordis/playground/jee/servlet/session/SessionServlet.java
@@ -0,0 +1,248 @@
+package io.novaordis.playground.jee.servlet.session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.ObjectName;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+
+/**
+ * @author <a href="mailto:ovidiu@novaordis.com">Ovidiu Feodorov</a>
+ *
+ * Copyright 2016 Nova Ordis LLC
+ */
+public class SessionServlet extends HttpServlet
+{
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    private static final Logger log = LoggerFactory.getLogger(SessionServlet.class);
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    // HttpServlet overrides -------------------------------------------------------------------------------------------
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+
+        String path = req.getPathInfo();
+        log.info("GET " + path);
+
+        //
+        // session handling
+        //
+
+        // look up session, but DO NOT create it if it's not there
+        HttpSession session = req.getSession(false);
+        log.info("HTTP session " + session);
+
+        // figure out whether we shall establish a session
+        if (shallWeEstablishSession(req)) {
+
+            if (session != null) {
+
+                // session already established - we don't support this case yet
+                throw new ServletException("HTTP session already established,  we don't currently re-process 'establish-session=true'");
+            }
+
+            // no session, create one
+            session = req.getSession(true);
+            log.info("created HTTP session " + session);
+        }
+
+        dumpHeaders(req);
+
+        String executingHost = getExecutingHost();
+
+        String user = req.getRemoteUser();
+        String remoteAddress = req.getRemoteAddr();
+        String remoteHost = req.getRemoteHost();
+
+        Cookie[] cookies = req.getCookies();
+
+        sendResponse(resp, path, executingHost, user, remoteAddress, remoteHost, cookies);
+    }
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    /**
+     * @param nodeName - can be null if the node name cannot be determined
+     */
+    private void sendResponse(HttpServletResponse r, String path, String nodeName, String user, String remoteAddress,
+                              String remoteHost, Cookie[] cookies) throws IOException {
+
+        r.setContentType("text/html");
+
+        PrintWriter out = r.getWriter();
+
+        out.println("<html>");
+        out.println("<head><title>Synthetic Response</title></head>");
+        out.println("<body>");
+        out.println("request successfully processed<br>");
+        out.println("<br>");
+        out.println("<tt>");
+
+        out.println("time:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                new Date() + "\n<br>");
+        out.println("path:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                path + "\n<br>");
+        out.println("executing host:&nbsp;&nbsp;&nbsp;" +
+                nodeName + "\n<br>");
+        out.println("remote user:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                user + "\n<br>");
+        out.println("remote address:&nbsp;&nbsp;&nbsp;" +
+                remoteAddress + "\n<br>");
+        out.println("remote host:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                remoteHost + "\n<br>");
+        out.println("received cookies:&nbsp;" +
+                (cookies == null || cookies.length == 0 ? "no cookies" : "") + "<br>\n");
+
+        if (cookies != null)
+        {
+            for(int i = 0; i < cookies.length; i ++)
+            {
+                out.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                        i + ") " + cookieToString(cookies[i]) + "<br>\n");
+            }
+        }
+        out.println("</tt>\n<br>");
+        out.println("</body></html>");
+    }
+
+    private void dumpHeaders(HttpServletRequest r) {
+
+        for(Enumeration e = r.getHeaderNames(); e.hasMoreElements(); ) {
+            String headerName = (String)e.nextElement();
+            log.info(headerName + "=" + r.getHeader(headerName));
+        }
+    }
+
+    /**
+     * @return null if the node name cannot be determined.
+     */
+    private String getExecutingHost() {
+
+        String name = null;
+
+        try {
+            name = queryJmxBus("jboss.system:type=ServerInfo", "HostName");
+        }
+        catch(Exception e)
+        {
+            log.error(e.getMessage());
+        }
+
+        // try one more time - this for tomcat
+
+        if (name == null)
+        {
+            try
+            {
+                InetAddress addr = InetAddress.getLocalHost();
+                return addr.getHostName();
+            }
+            catch(Exception e)
+            {
+                log.error("failed to get local host", e);
+            }
+        }
+
+        return name;
+    }
+
+    private String queryJmxBus(String objectName, String attributeName)
+    {
+        MBeanServer jbossMBeanServer = getJBossMBeanServer();
+
+        if (jbossMBeanServer == null)
+        {
+            log.debug("JBoss MBeanServer not found");
+            return null;
+        }
+
+        try
+        {
+            Object o = jbossMBeanServer.getAttribute(new ObjectName(objectName), attributeName);
+
+            if (o == null)
+            {
+                return null;
+            }
+
+            return o.toString();
+        }
+        catch(Exception e)
+        {
+            log.error("failed to query the JMX bus", e);
+        }
+
+        return null;
+    }
+
+    /**
+     * @return null if the JBoss MBeanServer not found.
+     */
+    private MBeanServer getJBossMBeanServer()
+    {
+        ArrayList<MBeanServer> servers = MBeanServerFactory.findMBeanServer(null);
+
+        for(MBeanServer s: servers)
+        {
+            if ("jboss".equals(s.getDefaultDomain()))
+            {
+                return s;
+            }
+        }
+
+        return null;
+    }
+
+    private String cookieToString(Cookie c)
+    {
+        StringBuffer sb = new StringBuffer();
+
+        sb.append(c.getName()).append("=").append(c.getValue());
+
+        String path = c.getPath();
+
+        sb.append(path == null ? " (no path)" : " " + path);
+
+        return sb.toString();
+    }
+
+    private boolean shallWeEstablishSession(HttpServletRequest r) {
+
+        String s = r.getParameter("establish-session");
+
+        if (s != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+}
