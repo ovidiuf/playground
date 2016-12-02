1acc093218b5b3ff38be58f5bd74c848bd6172d3
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-12-01 20:32:15 -0800

    slf4j example

--- slf4j/src/main/java/io/novaordis/playground/slf4j/Main.java
+++ slf4j/src/main/java/io/novaordis/playground/slf4j/Main.java
@@ -1,4 +1,23 @@
+/*
+ * Copyright (c) 2016 Nova Ordis LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.novaordis.playground.slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Ovidiu Feodorov <ovidiu@novaordis.com>
@@ -8,7 +27,14 @@ public class Main {
 
     // Constants -------------------------------------------------------------------------------------------------------
 
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     // Static ----------------------------------------------------------------------------------------------------------
+
+    public static void main(String[] args) throws Exception {
+
+        log.info("we are in main()");
+    }
 
     // Attributes ------------------------------------------------------------------------------------------------------
 
