c950876ee855120a6c17591162ca49d87bafb8b0
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-02-25 19:26:41 -0500

    working java to JSON jackson tree model example

---
+++ json/jackson/tree-model-java-to-json/README.md
@@ -0,0 +1,6 @@
+##Java to JSON - Jackson Tree Model Example
+
+##NOKB
+
+https://kb.novaordis.com/index.php/Jackson_Tree_Model#Java_to_JSON_with_Tree_Model_Code_Example
+
---
+++ json/jackson/tree-model-java-to-json/src/main/java/io/novaordis/playground/json/jackson/tree/java2json/Main.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2017 Nova Ordis LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.novaordis.playground.json.jackson.tree.java2json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.novaordis.playground.json.jackson.tree.java2json.model.Root;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * @author Ovidiu Feodorov <ovidiu@novaordis.com>
+ * @since 2/25/17
+ */
+public class Main {
+
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    public static void main(String[] args) throws Exception {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        ObjectMapper om = new ObjectMapper();
+
+        Root root = new Root();
+
+        om.writeValue(baos, root);
+
+        System.out.println(new String(baos.toByteArray()));
+    }
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+
+}
