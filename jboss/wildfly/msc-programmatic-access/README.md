57036757a21647edf5a774bf82c082efbf36f9c6
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-03-01 16:40:05 -0500

    WildFly MSC programmatic access

---
+++ jboss/wildfly/msc-programmatic-access/README.md
@@ -0,0 +1,5 @@
+#WildFly Modular Service Container Programmatic Access
+
+See https://kb.novaordis.com/index.php/WildFly_Modular_Service_Container_Programmatic_Access
+
+
---
+++ jboss/wildfly/msc-programmatic-access/src/main/java/io/novaordis/playground/jboss/wildfly/msc/MscUtil.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017 Nova Ordis LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.novaordis.playground.jboss.wildfly.msc;
+
+import org.jboss.as.server.CurrentServiceContainer;
+import org.jboss.msc.service.ServiceContainer;
+import org.jboss.msc.service.ServiceName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * @author Ovidiu Feodorov <ovidiu@novaordis.com>
+ * @since 3/1/17
+ */
+public class MscUtil {
+
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    private static final Logger log = LoggerFactory.getLogger(MscUtil.class);
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    public static void listServiceNames() {
+
+        ServiceName sn;
+
+        ServiceContainer sc = CurrentServiceContainer.getServiceContainer();
+
+        List<ServiceName> names = sc.getServiceNames();
+
+        for(ServiceName n: names) {
+
+            log.info(n.toString());
+        }
+    }
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    private MscUtil() {
+    }
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+
+}
