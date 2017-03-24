df59c695b6ca236a5b597e973a7dc8331fac52ea
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-03-24 15:33:13 -0400

    removed unused import

--- jee/ejb/stateless-and-servlet-different-deployment-units-same-jboss-instance/wrapper-servlet/src/main/java/io/novaordis/playground/jee/ejb/stateless/invoker/InvokerServlet.java
+++ jee/ejb/stateless-and-servlet-different-deployment-units-same-jboss-instance/wrapper-servlet/src/main/java/io/novaordis/playground/jee/ejb/stateless/invoker/InvokerServlet.java
@@ -20,7 +20,6 @@ import io.novaordis.playground.jee.ejb.stateless.SimpleStateless;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ejb.EJB;
 import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
