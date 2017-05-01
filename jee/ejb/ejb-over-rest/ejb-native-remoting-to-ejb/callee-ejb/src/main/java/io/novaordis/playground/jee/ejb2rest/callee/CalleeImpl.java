5564c44d471e97c9c1340850803745e4b98c8144
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-05-01 10:53:26 -0400

    EJB over REST: fully working native call

--- jee/ejb/ejb-over-rest/ejb-native-remoting-to-ejb/callee-ejb/src/main/java/io/novaordis/playground/jee/ejb2rest/callee/CalleeImpl.java
+++ jee/ejb/ejb-over-rest/ejb-native-remoting-to-ejb/callee-ejb/src/main/java/io/novaordis/playground/jee/ejb2rest/callee/CalleeImpl.java
@@ -1,62 +1,53 @@
-/*
- * Copyright (c) 2017 Nova Ordis LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package io.novaordis.playground.jee.ejb.ejb2rest.caller;
+package io.novaordis.playground.jee.ejb2rest.callee;
 
 import io.novaordis.playground.jee.ejb.ejb2rest.common.Callee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 /**
  * @author Ovidiu Feodorov <ovidiu@novaordis.com>
  * @since 5/1/17
  */
+@SuppressWarnings("unused")
 @Stateless
-public class CallerImpl implements LocalAccessToCaller {
+public class CalleeImpl implements Callee {
 
     // Constants -------------------------------------------------------------------------------------------------------
 
-    private static final Logger log = LoggerFactory.getLogger(CallerImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(CalleeImpl.class);
 
     // Static ----------------------------------------------------------------------------------------------------------
 
     // Attributes ------------------------------------------------------------------------------------------------------
 
-    @EJB(lookup = "ejb:/callee-ejb/CalleeImpl!io.novaordis.playground.jee.ejb.ejb2rest.common.Callee")
-    private Callee callee;
-
     // Constructors ----------------------------------------------------------------------------------------------------
 
-    // LocalAccessToCaller ---------------------------------------------------------------------------------------------
+    // Callee implementation -------------------------------------------------------------------------------------------
 
     @Override
-    public void triggerRemoteCall() {
+    public String businessMethodA(String arg) {
 
-        log.info("triggering remote call");
+        log.info(this + " got businessMethodA(" + arg + ")");
 
-        String result = callee.businessMethodA("test");
+        String result = "";
 
-        log.info("result: " + result);
+        for(int i = arg.length() - 1; i >= 0; i --) {
 
+            result += arg.charAt(i);
+        }
+
+        return result;
     }
 
     // Public ----------------------------------------------------------------------------------------------------------
+
+    @Override
+    public String toString() {
+
+        return "Callee[" + Integer.toHexString(System.identityHashCode(this)) + "]";
+    }
 
     // Package protected -----------------------------------------------------------------------------------------------
 
