35cf39f91b671953ff35430864ef4e2c7a3df10d
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-09-06 14:05:50 -0700

    added EAP 6 BOM example

---
+++ jboss/bom/eap6/src/main/java/io/novaordis/playground/jboss/bom/eap6/ServletExample.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2017 Nova Ordis LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.novaordis.playground.jboss.bom.eap6;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author Ovidiu Feodorov <ovidiu@novaordis.com>
+ * @since 9/6/17
+ */
+public class ServletExample extends HttpServlet {
+
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    // HttpServlet override --------------------------------------------------------------------------------------------
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        OutputStream os = resp.getOutputStream();
+
+        os.write("ok\n".getBytes());
+        os.flush();
+
+    }
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+
+}
