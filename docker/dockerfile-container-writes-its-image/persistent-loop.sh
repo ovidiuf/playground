66cb4e28d5403a87c7449c4b4f96c3f6e973b47b
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-12-04 19:15:24 -0800

    docker experiments

---
+++ docker/dockerfile-container-writes-its-image/persistent-loop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Must receive the following via environment variables:
+#
+# SUBJECT_NAME
+#
+
+#
+# read the value of the counter from the filesystem
+#
+
+cnt=$(cat /tmp/counter)
+[ -z "${cnt}" ] && cnt=0
+
+while [ true ]; do
+  echo "${cnt}"
+  cnt=$(expr ${cnt} + 1)
+  echo ${cnt} > /tmp/counter
+  sleep 1
+done
