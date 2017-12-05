7b3c468ae0c740594f879c349c893e4758482f26
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-12-04 17:55:58 -0800

    added environment variables

--- docker/simplest-dockerfile/loop.sh
+++ docker/simplest-dockerfile/loop.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#
+# Must receive the following via environment variables:
+#
+# SUBJECT_NAME
+#
 
 while [ true ]; do
-  echo ${cnt}
+  echo ${cnt} ${SUBJECT_NAME}
   cnt=$(expr ${cnt} + 1)
   sleep 1
 done
