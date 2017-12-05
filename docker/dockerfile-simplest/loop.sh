b036ef0efb4cd49fe327487d90e4cb8b649efc4a
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-12-04 18:22:27 -0800

    docker

--- docker/dockerfile-simplest/loop.sh
+++ docker/dockerfile-simplest/loop.sh
@@ -7,9 +7,10 @@
 
 echo_content=${SUBJECT_NAME}
 [ -z "${echo_content}" ] && echo_content="no 'SUBJECT_NAME' environment variable defined"
+cnt=0
 
 while [ true ]; do
-  echo ${cnt} ${echo_content}
+  echo "${cnt},  ${echo_content}"
   cnt=$(expr ${cnt} + 1)
   sleep 1
 done
