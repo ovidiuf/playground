50e9dd45be24abc319bafb2c9bed6d57606bb7de
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-12-08 19:28:51 -0800

    manual build

--- openshift/jenkins-plugin/Jenkinsfile-manual-build.groovy
+++ openshift/jenkins-plugin/Jenkinsfile-manual-build.groovy
@@ -1,38 +1,43 @@
 try {
+
     timeout(time: 20, unit: 'MINUTES') {
+
         def appName="${APP_NAME}"
         def project=""
 
-        node {
-            stage("Initialize") {
-                project = env.PROJECT_NAME
-            }
-        }
+        echo "appName: ${appName}"
 
-        node("maven") {
-            stage("Checkout") {
-                git url: "${GIT_SOURCE_URL}", branch: "${GIT_SOURCE_REF}"
-            }
-            stage("Build WAR") {
-                sh "mvn clean package -Popenshift"
-                stash name:"war", includes:"target/ROOT.war"
-            }
-        }
-
-        node {
-            stage("Build Image") {
-                unstash name:"war"
-                sh "oc start-build ${appName}-docker --from-file=target/ROOT.war -n ${project}"
-                openshiftVerifyBuild bldCfg: "${appName}-docker", namespace: project, waitTime: '20', waitUnit: 'min'
-            }
-            stage("Deploy") {
-                openshiftDeploy deploymentConfig: appName, namespace: project
-            }
-        }
+//        node {
+//            stage("Initialize") {
+//                project = env.PROJECT_NAME
+//            }
+//        }
+//
+//        node("maven") {
+//            stage("Checkout") {
+//                git url: "${GIT_SOURCE_URL}", branch: "${GIT_SOURCE_REF}"
+//            }
+//            stage("Build WAR") {
+//                sh "mvn clean package -Popenshift"
+//                stash name:"war", includes:"target/ROOT.war"
+//            }
+//        }
+//
+//        node {
+//            stage("Build Image") {
+//                unstash name:"war"
+//                sh "oc start-build ${appName}-docker --from-file=target/ROOT.war -n ${project}"
+//                openshiftVerifyBuild bldCfg: "${appName}-docker", namespace: project, waitTime: '20', waitUnit: 'min'
+//            }
+//            stage("Deploy") {
+//                openshiftDeploy deploymentConfig: appName, namespace: project
+//            }
+//        }
     }
-} catch (err) {
-    echo "in catch block"
-    echo "Caught: ${err}"
+}
+catch (err) {
+
+    echo "build failure: ${err}"
     currentBuild.result = 'FAILURE'
     throw err
 }
