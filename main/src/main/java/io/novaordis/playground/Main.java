c2886a372001921b763cc29559385618ac6e40f9
Ovidiu Feodorov <ovidiu@novaordis.com>
2018-05-06 01:09:37 -0700

    misc

--- main/src/main/java/io/novaordis/playground/Main.java
+++ main/src/main/java/io/novaordis/playground/Main.java
@@ -1,75 +1,32 @@
 package io.novaordis.playground;
 
-import java.io.FileWriter;
-import java.io.RandomAccessFile;
-import java.util.Date;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Main {
 
-    // Constants -------------------------------------------------------------------------------------------------------
-
-    // Static ----------------------------------------------------------------------------------------------------------
 
     public static void main(String[] args) throws Exception {
 
-        if (args.length == 0) {
 
-            truncateFile();
-
-        }
-        else if ("write".equals(args[0])) {
-
-            writeFile();
-        }
-        else {
-
-            throw new Exception("unknown command " + args[0]);
-        }
-    }
-    // Attributes ------------------------------------------------------------------------------------------------------
-
-    // Constructors ----------------------------------------------------------------------------------------------------
-
-    // Public ----------------------------------------------------------------------------------------------------------
-
-    // Package protected -----------------------------------------------------------------------------------------------
-
-    // Protected -------------------------------------------------------------------------------------------------------
-
-    // Private ---------------------------------------------------------------------------------------------------------
-
-    private static void writeFile() throws Exception {
-
-        FileWriter fw = new FileWriter("./test.txt");
-
-        while(true) {
-
-            System.out.print(".");
-            fw.write(new Date().toString() + "\n");
-            fw.flush();
-            Thread.sleep(1000L);
-        }
-
-    }
-
-    private static void truncateFile() throws Exception {
-
-
-        RandomAccessFile raf = new RandomAccessFile("./test.txt", "rws");
-
-        long pointer = raf.getFilePointer();
-
-        System.out.println("pointer: " + pointer);
-
-        raf.setLength(0L);
-        raf.close();
-
-        System.out.println("truncated");
+        System.out.println(sumFirstNNumbers(Integer.parseInt(args[0])));
 
     }
 
 
-    // Inner classes ---------------------------------------------------------------------------------------------------
+
+
+    public static long sumFirstNNumbers(int n) {
+
+        Optional<Long> result =
+                Stream.
+                        iterate(1L, prev -> prev + 1).
+                        limit(n).
+                        parallel().
+                        reduce(Long::sum);
+
+        return result.get();
+    }
 
 
 }
