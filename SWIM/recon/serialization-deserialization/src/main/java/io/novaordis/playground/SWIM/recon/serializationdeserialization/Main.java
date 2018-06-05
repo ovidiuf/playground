dd8089efe994c2be47e72fbb986554af19982e13
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-06-05 13:39:25 -0700

    RECON (1)

--- SWIM/recon/serialization-deserialization/src/main/java/io/novaordis/playground/SWIM/recon/serializationdeserialization/Main.java
+++ SWIM/recon/serialization-deserialization/src/main/java/io/novaordis/playground/SWIM/recon/serializationdeserialization/Main.java
@@ -6,12 +6,89 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Value v = Value.parseRecon("test");
+        //
+        // RECON text to and from RECON Objects
+        //
 
-        System.out.println(v);
+        if (args.length == 0) {
 
-        String s = v.toRecon();
+            System.err.println("[error]: specify an argument that will be converted to RECON");
+            return;
+        }
 
-        System.out.println(s);
+        String arg = args[0];
+
+        System.out.println("argument >" + arg + "<");
+
+        Value v = Value.parseRecon(arg);
+
+        System.out.println(v.getClass() + ": " + v);
+
+        //
+        // RECON text to and from RECON Objects
+        //
+
+        try {
+
+            System.out.println("boolean value: " + v.booleanValue());
+        }
+        catch(Exception e) {
+
+            System.out.println(v + " cannot be converted to boolean: " + e);
+        }
+
+        try {
+
+            System.out.println("char value:    " + v.charValue());
+        }
+        catch(Exception e) {
+
+            System.out.println(v + " cannot be converted to char: " + e);
+        }
+
+        try {
+
+            System.out.println("int value:     " + v.intValue());
+        }
+        catch(Exception e) {
+
+            System.out.println(v + " cannot be converted to int: " + e);
+        }
+
+        try {
+
+            System.out.println("long value:    " + v.longValue());
+        }
+        catch(Exception e) {
+
+            System.out.println(v + " cannot be converted to long: " + e);
+        }
+
+        try {
+
+            System.out.println("float value:   " + v.floatValue());
+        }
+        catch(Exception e) {
+
+            System.out.println(v + " cannot be converted to float: " + e);
+        }
+
+        try {
+
+            System.out.println("double value:  " + v.doubleValue());
+        }
+        catch(Exception e) {
+
+            System.out.println(v + " cannot be converted to double: " + e);
+        }
+
+        try {
+
+            System.out.println("string value:  " + v.stringValue());
+        }
+        catch(Exception e) {
+
+            System.out.println(v + " cannot be converted to string: " + e);
+        }
     }
 }
