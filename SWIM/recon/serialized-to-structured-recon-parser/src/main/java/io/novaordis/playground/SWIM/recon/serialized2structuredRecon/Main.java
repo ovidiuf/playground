eb78e4ade66b4acdcb28c17c81a92e8ba776f22f
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-06-07 16:02:18 -0700

    parser output tweaks

--- SWIM/recon/serialized-to-structured-recon-parser/src/main/java/io/novaordis/playground/SWIM/recon/serialized2structuredRecon/Main.java
+++ SWIM/recon/serialized-to-structured-recon-parser/src/main/java/io/novaordis/playground/SWIM/recon/serialized2structuredRecon/Main.java
@@ -7,20 +7,32 @@ import recon.Slot;
 import recon.Value;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        String serializedInput = args[0];
+        Configuration c = new Configuration(args);
 
-        System.out.println("serialized input: >" + serializedInput + "<");
+        String serializedInput = c.getSerializedInput();
+
+        String s = "input: ";
+
+        if (serializedInput.contains("\n")) {
+
+            s = "multi-line " + s + "\n";
+        }
+
+        s += serializedInput;
+
+        System.out.println("\n" + s);
 
         Value value = Value.parseRecon(serializedInput);
 
         System.out.println("\nstructured RECON:\n");
 
-        display(value, 4);
+        recursivelyDisplayOnMultipleLines(value, 4);
 
         String serialized = value.toRecon();
 
@@ -28,13 +40,17 @@ public class Main {
 
     }
 
-    private static void display(Item item, int indentation) {
+    private static void recursivelyDisplayOnMultipleLines(Item item, int indentation) {
 
         String leader = getLeader(indentation);
 
         if (item instanceof Record) {
 
-            System.out.println(leader + item.getClass().getName() + ": ");
+            String type = item.getClass().getSimpleName();
+
+            type = "RecordMap".equals(type) ? "Record" : type;
+
+            System.out.println(leader + type + " { ");
 
             //
             // iterate
@@ -42,40 +58,100 @@ public class Main {
 
             for(Item i: item) {
 
-                display(i, indentation + 2);
+                recursivelyDisplayOnMultipleLines(i, indentation + 2);
             }
+
+            System.out.println(leader + "}");
         }
         else {
 
-            if (item.isAbsent()) {
-
-                System.out.println(leader + "ABSENT");
-            }
-            else if (item.isExtant()) {
-
-                System.out.println(leader + "EXTANT");
-            }
-            else if (item.isBool() || item.isText() || item.isData() || item.isNumber()) {
-
-                System.out.println(leader + item.getClass().getName() + ": " + item.stringValue());
-            }
-            else if (item.isAttr()) {
-
-                Attr attr = (Attr)item;
-
-                System.out.println(leader + attr.getClass().getName() + ": " + attr.getName() + "=" + attr.getValue());
-            }
-            else if (item.isSlot()) {
-
-                Slot slot = (Slot)item;
-
-                System.out.println(leader + slot.getClass().getName() + ": " + slot.getKey() + "=" + slot.getValue());
-            }
-            else {
-
-                throw new IllegalArgumentException("unknown type: " + item);
-            }
+            System.out.println(leader + toLiteral(item));
         }
+    }
+
+    /**
+     * A "literal" representation of the Item.
+     */
+    private static String toLiteral(Item item) {
+
+        StringBuilder sb = new StringBuilder();
+
+        if (item instanceof Record) {
+
+            String type = item.getClass().getSimpleName();
+
+            type = "RecordMap".equals(type) ? "Record" : type;
+
+            sb.append(type).append(" {");
+
+            //
+            // iterate
+            //
+
+            for(Iterator<Item> it = item.iterator(); it.hasNext(); ) {
+
+                Item i = it.next();
+
+                sb.append(toLiteral(i));
+
+                if (it.hasNext()) {
+
+                    sb.append(", ");
+                }
+            }
+
+            sb.append("}");
+        }
+        else if (item.isAbsent()) {
+
+            sb.append("ABSENT");
+        }
+        else if (item.isExtant()) {
+
+            sb.append("EXTANT");
+        }
+        else if (item.isText()) {
+
+            //
+            // unlike booleans, data and numbers, we want to be able to represent leading or trailing whitespaces
+            //
+
+            sb.append(item.getClass().getSimpleName()).
+                    append(" '").
+                    append(item.stringValue()).
+                    append("'");
+        }
+        else if (item.isBool() || item.isData() || item.isNumber()) {
+
+            sb.append(item.getClass().getSimpleName()).
+                    append(" ").
+                    append(item.stringValue());
+        }
+        else if (item.isAttr()) {
+
+            Attr attr = (Attr)item;
+            sb.append(attr.getClass().getSimpleName()).
+                    append(" ").
+                    append(attr.getName()).
+                    append(" (").
+                    append(toLiteral(attr.getValue())).
+                    append(")");
+        }
+        else if (item.isSlot()) {
+
+            Slot slot = (Slot)item;
+            sb.append(slot.getClass().getSimpleName()).
+                    append(" ").
+                    append(toLiteral(slot.getKey())).
+                    append(": ").
+                    append(toLiteral(slot.getValue()));
+        }
+        else {
+
+            throw new IllegalArgumentException("unknown type: " + item);
+        }
+
+        return sb.toString();
     }
 
     private static String getLeader(int indentationCharacters) {
