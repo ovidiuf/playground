5de8cc0287c04c0e1509a52f192c48fe8f33f853
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-06-07 16:06:20 -0700

    work

---
+++ SWIM/recon/structured-recon-to-serialized-workbench/src/main/java/io/novaordis/playground/SWIM/recon/structuredReconToSerializedWorkbench/Main.java
@@ -0,0 +1,22 @@
+package io.novaordis.playground.SWIM.recon.structuredReconToSerializedWorkbench;
+
+import recon.Attr;
+import recon.Record;
+import recon.Slot;
+import recon.Value;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Attr attr = Attr.of("color");
+
+        final Record record = Record.of().slot("blah", "something");
+
+        String serialized = record.toRecon();
+
+        System.out.println(serialized);
+
+
+    }
+}
