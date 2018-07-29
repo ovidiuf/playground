a9e8dff3cbf26f7eb6fb16e425e6f8c0c2e60b3f
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-07-28 17:13:24 -0700

    SWIM service

---
+++ SWIM/service/src/main/java/io/novaordis/playground/SWIM/service/client/ClientMain.java
@@ -0,0 +1,13 @@
+package io.novaordis.playground.SWIM.service.client;
+
+/**
+ * @author Ovidiu Feodorov <ovidiu@swim.ai>
+ * @since 7/28/18
+ */
+public class ClientMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("client");
+    }
+}
---
+++ SWIM/service/src/main/java/io/novaordis/playground/SWIM/service/server/ServerMain.java
@@ -0,0 +1,13 @@
+package io.novaordis.playground.SWIM.service.server;
+
+/**
+ * @author Ovidiu Feodorov <ovidiu@swim.ai>
+ * @since 7/28/18
+ */
+public class ServerMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("server");
+    }
+}
