403aece2dea26e2d183a0a8b6c885786ea27c72e
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-08-09 15:20:57 -0700

    dsa

--- data-structures-and-algorithms/maximum-subarray-problem/src/main/java/playground/dsa/maximumSubarrayProblem/MaxProfit.java
+++ data-structures-and-algorithms/maximum-subarray-problem/src/main/java/playground/dsa/maximumSubarrayProblem/MaxProfit.java
@@ -3,31 +3,31 @@ package playground.dsa.maximumSubarrayProblem;
 public class MaxProfit {
 
     /**
-     * @return an int[3] with { buyDateIndex, sellDateIndex, profit}
+     * @return an int[3] with {buyDateIndex, sellDateIndex, profit}
      */
     public int[] bruteForce(int[] price) {
 
         int maxProfit = Integer.MIN_VALUE;
-        int b = -1;
-        int s = -1;
+        int buy = -1; // the index of the day we should buy
+        int sell = -1; // the index of the day we should sell
 
         for(int i = 0; i < price.length; i ++) {
 
             for(int j = i + 1; j < price.length; j ++) {
 
-
+                // we only compare with price in subsequent days
                 int profit = price[j] - price[i];
 
                 if (profit > maxProfit) {
 
                     maxProfit = profit;
-                    b = i;
-                    s = j;
+                    buy = i;
+                    sell = j;
                 }
             }
         }
 
-        return new int[] {b, s, maxProfit};
+        return new int[] {buy, sell, maxProfit};
     }
 
     public int[] priceToDiff(int[] price) {
