e12ea4d3612ea6132f271e7f1a72ddcc626a7744
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-10-07 20:54:12 -0700

    spring playground, first commit

---
+++ spring/spring-boot-with-initializr-from-intellij/src/main/java/playground/spring/springBootWithInitializrFromIntellij/AppApplication.java
@@ -0,0 +1,12 @@
+package playground.spring.springBootWithInitializrFromIntellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
