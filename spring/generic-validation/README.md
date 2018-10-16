f8c3b168af16449e12ce06fef1b86b51fa463478
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-10-15 18:08:28 -0700

    generic javabeans validation

---
+++ spring/generic-validation/README.md
@@ -0,0 +1,3 @@
+# Generic JavaBean Validation Example
+
+For details, see https://kb.novaordis.com/index.php/Generic_JavaBeans_Validation#Overview.
---
+++ spring/generic-validation/src/main/java/playground/spring/genericvalidation/GenericValidationApplication.java
@@ -0,0 +1,12 @@
+package playground.spring.genericvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericValidationApplication.class, args);
+	}
+}
