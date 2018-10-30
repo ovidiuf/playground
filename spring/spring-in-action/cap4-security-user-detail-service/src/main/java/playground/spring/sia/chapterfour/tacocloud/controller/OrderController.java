16c27e7c778f0e1a0cb34248a4fb642e487cef74
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-10-30 10:14:27 -0700

    sia user detail service

---
+++ spring/spring-in-action/cap4-security-user-detail-service/src/main/java/playground/spring/sia/chapterfour/tacocloud/controller/OrderController.java
@@ -0,0 +1,49 @@
+package playground.spring.sia.chapterfour.tacocloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import playground.spring.sia.chapterfour.tacocloud.model.Order;
+import playground.spring.sia.chapterfour.tacocloud.persistence.OrderRepository;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("order")
+public class OrderController {
+
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepository) {
+
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping("/current")
+    public String renderOrderForm() {
+
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+
+        if (errors.hasErrors()) {
+
+            return "orderForm";
+        }
+
+        orderRepository.save(order);
+
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+}
---
+++ spring/spring-in-action/cap4-security-user-detail-service/src/main/java/playground/spring/sia/chapterfour/tacocloud/controller/TacoDesignController.java
@@ -0,0 +1,96 @@
+package playground.spring.sia.chapterfour.tacocloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import playground.spring.sia.chapterfour.tacocloud.model.Ingredient;
+import playground.spring.sia.chapterfour.tacocloud.model.Order;
+import playground.spring.sia.chapterfour.tacocloud.model.Taco;
+import playground.spring.sia.chapterfour.tacocloud.persistence.IngredientRepository;
+import playground.spring.sia.chapterfour.tacocloud.persistence.TacoRepository;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("order")
+public class TacoDesignController {
+
+    private final IngredientRepository ingredientRepository;
+    private final TacoRepository tacoDesignRepository;
+
+    @Autowired
+    public TacoDesignController(IngredientRepository ingredientRepository, TacoRepository tacoDesignRepository) {
+
+        this.ingredientRepository = ingredientRepository;
+        this.tacoDesignRepository = tacoDesignRepository;
+    }
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+
+        return new Taco();
+    }
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+
+        List<Ingredient> ingredients = new ArrayList<>();
+
+        ingredientRepository.findAll().forEach(ingredients::add);
+
+        Ingredient.Type[] types = Ingredient.Type.values();
+
+        for(Ingredient.Type type: types) {
+
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+
+        model.addAttribute("taco", new Taco());
+
+        return "design";
+    }
+
+    @PostMapping
+    public String processTacoDesign(
+            @Valid Taco taco, Errors errors, @ModelAttribute Order order) {
+
+        if (errors.hasErrors()) {
+
+            System.out.println("we have errors in the taco design!");
+
+            return "design";
+        }
+
+        // save the taco design
+
+        Taco saved = tacoDesignRepository.save(taco);
+        order.addTacoDesign(saved);
+
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
+
+        List<Ingredient> result = new ArrayList<>();
+
+        for(Ingredient i: ingredients) {
+
+            if (type.equals(i.getType())) {
+
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
