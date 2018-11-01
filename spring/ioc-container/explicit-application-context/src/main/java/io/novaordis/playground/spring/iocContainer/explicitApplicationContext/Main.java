14d77f11d4f999c9195add034facd6fec2e2e510
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-11-01 14:52:11 -0700

    dependency management in place

---
+++ spring/ioc-container/explicit-application-context/src/main/java/io/novaordis/playground/spring/iocContainer/explicitApplicationContext/Main.java
@@ -0,0 +1,9 @@
+package io.novaordis.playground.spring.iocContainer.explicitApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(".");
+    }
+}
--- spring/ioc-container/explicit-application-context/src/main/java/playground/spring/ioccontainer/explicitapplicationcontext/Main.java
+++
@@ -1,9 +0,0 @@
-package playground.spring.ioccontainer.explicitapplicationcontext;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-
-        System.out.println(".");
-    }
-}
