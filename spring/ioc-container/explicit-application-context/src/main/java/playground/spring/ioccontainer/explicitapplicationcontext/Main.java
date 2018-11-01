11e1a1943d8105dd88eaf53ecd4fea3d7d6499be
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-11-01 14:48:48 -0700

    deleted eac

---
+++ spring/ioc-container/explicit-application-context/src/main/java/playground/spring/ioccontainer/explicitapplicationcontext/Main.java
@@ -0,0 +1,9 @@
+package playground.spring.ioccontainer.explicitapplicationcontext;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(".");
+    }
+}
