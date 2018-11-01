fa688bd1d6210b19082801e44bc890b031756bbe
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-10-31 18:44:21 -0700

    got rid of unused classes

--- spring/jpa/attribute-converter/src/main/java/playground/spring/jpa/attributeConverter/command/Command.java
+++
@@ -1,8 +0,0 @@
-package playground.spring.jpa.attributeConverter.command;
-
-import playground.spring.jpa.attributeConverter.repository.ARepository;
-
-public interface Command {
-
-    void execute(ARepository aRepository) throws Exception;
-}
