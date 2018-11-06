8f13f8b2e65d06bbd9e8c39d2942112244819157
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-11-05 18:36:23 -0800

    spring

---
+++ spring/ioc-container/01.1-java-based-configuration-with-xml-configuration/README.md
@@ -0,0 +1,5 @@
+# Spring Inversion of Control Container
+
+Java-based configuration from an XML-based configuration application context.
+
+NOKB: https://kb.novaordis.com/index.php/Spring_Dependency_Injection_and_Inversion_of_Control_Container_Concepts#Using_Java-based_Configuration_from_an_XML-based-configuration_Application_Context
---
+++ spring/ioc-container/01.1-java-based-configuration-with-xml-configuration/src/main/java/playground/Main.java
@@ -0,0 +1,20 @@
+package playground;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SuppressWarnings("WeakerAccess")
+public class Main {
+
+    static ApplicationContext APPLICATION_CONTEXT = new ClassPathXmlApplicationContext("beans.xml");
+
+    public static void main(String[] args) {
+
+        Blue blue = (Blue)APPLICATION_CONTEXT.getBean("blue");
+
+        blue.run();
+
+
+    }
+}
