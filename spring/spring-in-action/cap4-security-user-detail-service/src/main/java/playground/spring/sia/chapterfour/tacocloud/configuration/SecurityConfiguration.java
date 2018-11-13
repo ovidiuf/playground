cf752ff5df83fe098ef9eecdb096119058dd77ac
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-11-12 21:57:39 -0800

    ...

--- spring/spring-in-action/cap4-security-user-detail-service/src/main/java/playground/spring/sia/chapterfour/tacocloud/configuration/SecurityConfiguration.java
+++ spring/spring-in-action/cap4-security-user-detail-service/src/main/java/playground/spring/sia/chapterfour/tacocloud/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,6 +34,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public PasswordEncoder encoder() {
 
         return new StandardPasswordEncoder("something");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().
+                antMatchers("/design", "/orders").hasRole("ROLE_USER").
+                antMatchers("/", "/**").permitAll();
     }
 
 }
--- spring/spring-in-action/cap4-security-user-detail-service/src/main/java/playground/spring/sia/chapterfour/tacocloud/configuration/TacoCloudConfiguration.java
+++ spring/spring-in-action/cap4-security-user-detail-service/src/main/java/playground/spring/sia/chapterfour/tacocloud/configuration/TacoCloudConfiguration.java
@@ -11,5 +11,6 @@ public class TacoCloudConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
 
         registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login");
     }
 }
