82492f64591a8f91e48e66e1a193748cc1c14c48
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-11-23 13:40:53 -0800

    attempt to make property loading in a non-Spring Boot app

--- spring/testing/02-integration-test-with-config-property-holder/src/main/java/playground/spring/testing/AComponent.java
+++ spring/testing/02-integration-test-with-config-property-holder/src/main/java/playground/spring/testing/AComponent.java
@@ -1,9 +1,11 @@
 package playground.spring.testing;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("a")
 public class AComponent {
 
     private AComponentPropertyConfiguration propertyConfiguration;
