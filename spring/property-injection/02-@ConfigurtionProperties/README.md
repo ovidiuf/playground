35e1d483f737452b5c046b7d7f5f4a0932cb07a0
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-12-02 00:42:50 -0800

    config

---
+++ spring/property-injection/02-@ConfigurtionProperties/README.md
@@ -0,0 +1,4 @@
+# Spring and Property Injection
+
+NOKB: https://kb.novaordis.com/index.php/Spring_Property_Injection_Concepts
+
---
+++ spring/property-injection/02-@ConfigurtionProperties/src/main/java/playground/spring/propertyinjection/environmentandpropertysources/CommandLineLoop.java
@@ -0,0 +1,38 @@
+package playground.spring.propertyinjection.environmentandpropertysources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@SuppressWarnings("WeakerAccess")
+@Component
+@ConfigurationProperties(prefix = "playground.spring.pi")
+public class CommandLineLoop {
+
+    public static final int DEFAULT_WEIGHT = 50;
+
+    private int weight = DEFAULT_WEIGHT;
+
+    private MyPropertyConfiguration propertyConfiguration;
+
+    @Autowired
+    private CommandLineLoop(MyPropertyConfiguration propertyConfiguration) {
+
+        this.propertyConfiguration = propertyConfiguration;
+    }
+
+    public void setWeight(int weight) {
+
+        this.weight = weight;
+    }
+
+    void run(String... args) {
+
+        System.out.println("");
+        System.out.println("  color: " + propertyConfiguration.getColor());
+        System.out.println("   size: " + propertyConfiguration.getSize());
+        System.out.println("");
+        System.out.println(" weight: " + weight);
+        System.out.println("");
+    }
+}
