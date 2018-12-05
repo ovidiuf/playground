cf9a47675b7d0026c2e52a7993c58259272299ef
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-12-05 11:52:41 -0800

    ...

--- spring/testing/02-integration-test-with-config-property-holder/README.md
+++ spring/testing/02-integration-test-with-config-property-holder/README.md
@@ -1,3 +1,6 @@
-# Spring TestContext Framework and Properties Playground 
+# Spring TestContext Framework and Properties Configuration Holders
 
-NOKB https://kb.novaordis.com/index.php/Spring_Framework_Testing_Concepts#Integration_Test_Programming_Model
+Property configuration holders are a Spring Boot pattern. This examples shows how to use
+them with plain Spring TestContext.
+
+NOKB https://kb.novaordis.com/index.php?title=Using_Property_Configuration_Holders_with_Plain_Spring_TestContext_Framework&action=edit&redlink=1
--- spring/testing/02-integration-test-with-config-property-holder/src/test/java/playground/spring/testing/integration/AComponentIntegrationTests.java
+++ spring/testing/02-integration-test-with-config-property-holder/src/test/java/playground/spring/testing/integration/AComponentIntegrationTests.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import playground.spring.testing.AComponent;
 import playground.spring.testing.AComponentPropertyConfiguration;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
@@ -27,5 +28,7 @@ public class AComponentIntegrationTests {
         String name = aComponent.getPropertyConfiguration().getName();
 
         System.out.println(name);
+
+        assertEquals("yellow", name);
     }
 }
