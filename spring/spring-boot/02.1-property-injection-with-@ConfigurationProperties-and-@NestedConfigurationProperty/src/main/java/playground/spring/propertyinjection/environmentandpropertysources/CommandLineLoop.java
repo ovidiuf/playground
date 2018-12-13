d0aac550e63cce3eb789f76c9187081223ecdb3e
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-12-13 10:37:36 -0800

    ...

--- spring/spring-boot/02.1-property-injection-with-@ConfigurationProperties-and-@NestedConfigurationProperty/src/main/java/playground/spring/propertyinjection/environmentandpropertysources/CommandLineLoop.java
+++ spring/spring-boot/02.1-property-injection-with-@ConfigurationProperties-and-@NestedConfigurationProperty/src/main/java/playground/spring/propertyinjection/environmentandpropertysources/CommandLineLoop.java
@@ -2,6 +2,7 @@ package playground.spring.propertyinjection.environmentandpropertysources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @SuppressWarnings("WeakerAccess")
@@ -15,10 +16,13 @@ public class CommandLineLoop {
 
     private MyPropertyConfiguration propertyConfiguration;
 
+    private Environment environment;
+
     @Autowired
-    private CommandLineLoop(MyPropertyConfiguration propertyConfiguration) {
+    private CommandLineLoop(MyPropertyConfiguration propertyConfiguration, Environment environment) {
 
         this.propertyConfiguration = propertyConfiguration;
+        this.environment = environment;
     }
 
     public void setWeight(int weight) {
@@ -28,9 +32,16 @@ public class CommandLineLoop {
 
     void run(String... args) {
 
+        String s = environment.getProperty("playground.spring.pi.stuff.weight");
+
+        assert "55".equals(s);
+
         System.out.println("");
         System.out.println("   size: " + propertyConfiguration.getSize());
         System.out.println("   stuff.weight: " + propertyConfiguration.getStuff().getWeight());
         System.out.println("");
+
+
+
     }
 }
