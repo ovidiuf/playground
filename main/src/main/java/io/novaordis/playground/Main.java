d10fb2124aae56e75a41944eab6a081dc192a60b
Ovidiu Feodorov <ovidiu@feodorov.com>
2018-12-17 11:42:07 -0800

    changing names

--- main/src/main/java/io/novaordis/playground/Main.java
+++ main/src/main/java/io/novaordis/playground/Main.java
@@ -1,89 +1,20 @@
+/*
+ * Copyright (c) 2018 Nova Ordis LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.novaordis.playground;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 public class Main {
-
-
-    //private []Integer test =
-
-
-    public static void main(String[] args) throws Exception {
-
-
-        MyHashMapImpl map = new MyHashMapImpl();
-//        map.put(5, 25);
-//        System.out.println(map.get(5));
-//        map.put(5, 36);
-//        System.out.println(map.get(5));
-//        map.put(8, 64);
-//        System.out.println(map.get(8));
-
-//        for(int i = 0; i < 100; i ++) {
-//
-//            System.out.println("key: " + i + " hash: " + map.getBucket(map.getHashCode(i)));
-//        }
-
-        map.put(1, 10);
-        map.put(16, 20);
-
-        System.out.println(map.get(1));
-
-    }
-
-
-    private static int logicalAnd(int i, int size) {
-
-        return i & (size - 1);
-    }
-
-    static interface MyHashMap {
-
-        public Object get(Object key);
-        public void put (Object key, Object Val);
-    }
-
-    static class MyHashMapImpl implements MyHashMap {
-
-        Object[] keys;
-        Object[] values;
-        int size = 16;
-
-        public MyHashMapImpl() {
-            keys = new Object[size];
-            values = new Object[size];
-        }
-
-        public int getHashCode(int hash) {
-
-            hash ^= (hash >>> 20) ^ (hash >>> 12);
-            return hash ^ (hash >>> 7) ^ (hash >>> 4);
-
-        }
-
-        int getBucket(int hash) {
-            return hash & (this.size - 1);
-        }
-
-
-        @Override
-        public Object get(Object key) {
-            int hash = getHashCode(key.hashCode());
-            int bucket = getBucket(hash);
-            return values[bucket];
-        }
-
-        @Override
-        public void put(Object key, Object val) {
-            int bucket = getBucket(getHashCode(key.hashCode()));
-            keys[bucket] = key;
-            values[bucket] = val;
-        }
-    }
-
 }
