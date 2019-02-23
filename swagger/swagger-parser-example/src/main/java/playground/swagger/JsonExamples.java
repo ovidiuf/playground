f4a5ef5b755bea92325fbda5c71c3da9eb14ccf8
Ovidiu Feodorov <ovidiu@feodorov.com>
2019-02-22 18:12:16 -0800

    ...

--- swagger/swagger-parser-example/src/main/java/playground/swagger/JsonExamples.java
+++ swagger/swagger-parser-example/src/main/java/playground/swagger/JsonExamples.java
@@ -13,5 +13,5 @@ public class JsonExamples {
 
         return Json.mapper().writeValueAsString(swagger);
     }
-    
+
 }
--- swagger/swagger-parser-example/src/main/java/playground/swagger/Main.java
+++ swagger/swagger-parser-example/src/main/java/playground/swagger/Main.java
@@ -23,7 +23,5 @@ public class Main {
 
         String s = JsonExamples.convertToJsonString(swagger);
         System.out.println(s);
-
-
     }
 }
