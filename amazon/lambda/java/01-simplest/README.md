97ef50fe1b4963b8cf0b31d20cea9d6a43da932d
Ovidiu Feodorov <ovidiu@feodorov.com>
2019-03-07 14:00:50 -0800

    simplest lambda

---
+++ amazon/lambda/java/01-simplest/README.md
@@ -0,0 +1,5 @@
+# Simplest AWS Java Lambda
+
+# NOKB
+
+[NOKB Java Lambda](https://kb.novaordis.com/index.php/AWS_Java_Lambda_Development#Overview)
---
+++ amazon/lambda/java/01-simplest/src/main/java/playground/amazon/lambda/Main.java
@@ -0,0 +1,9 @@
+package playground.amazon.lambda;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(".");
+    }
+}
