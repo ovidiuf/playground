ebef654950c0d93160c915a13f830137063fa4b9
Ovidiu Feodorov <ovidiu@feodorov.com>
2019-03-20 13:37:02 -0700

    Gradle command line property experiments

---
+++ gradle/passing-configuration-on-command-line/README.md
@@ -0,0 +1,3 @@
+# Gradle Pass Configuration on Command Line
+
+NOKB: https://kb.novaordis.com/index.php/Gradle_Pass_Configuration_on_Command_Line
---
+++ gradle/passing-configuration-on-command-line/src/main/java/playground/gradle/Main.java
@@ -0,0 +1,9 @@
+package playground.gradle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(".");
+    }
+}
