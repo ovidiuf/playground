6f46f0fab446ae616451706b462f92baeb0068a7
Ovidiu Feodorov <ovidiu@feodorov.com>
2019-09-25 11:14:20 -0700

    helm

--- helm/configuration/install.sh
+++ helm/configuration/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 helm dependency update ./a
-helm install --debug --name config-example ./a
+helm install --debug --name config-example -f ./file-based-overrides.yaml ./a
+#helm install --debug --name config-example --set-file ./set-file-example.yaml ./a
 
 
