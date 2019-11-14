90e21d54e04323750890b065c7bcbd521676fc56
Ovidiu Feodorov <ovidiu@feodorov.com>
2019-11-13 19:12:36 -0800

    terraform

--- hashicorp/terraform/blue-module/main.tf
+++ hashicorp/terraform/blue-module/main.tf
@@ -1,3 +1,10 @@
+#
+# Input variables are declared in variables.tf
+#
+
+#
+# Output variables are declared in outpus.tf
+#
 
 #
 # Constrain Terraform version in case version-specific features are used
@@ -6,10 +13,55 @@ terraform {
   required_version = ">= 0.12"
 }
 
+provider "aws" {
+
+  profile = "default"
+  region  = "us-west-2"
+}
+
 #
 # Declare resources here
 #
 
+#
+# Masters - Control Plane
+#
+
+resource "aws_instance" "master-node" {
+
+  count                       = "${var.masters}"
+  ami                         = "${var.ami_id}"
+  instance_type               = "${var.master_instance_type}"
+  subnet_id                   = "${var.subnet_id}"
+  key_name                    = "${var.ssh_key_name}"
+  security_groups             = "${var.security_groups}"
+  associate_public_ip_address = false
+
+  tags = {
+
+    Name = "kubernetes-master-${count.index + 1}"
+  }
+}
+
+#
+# Workers - Data Plane
+#
+
+resource "aws_instance" "worker-node" {
+
+  count                       = "${var.workers}"
+  ami                         = "${var.ami_id}"
+  instance_type               = "${var.worker_instance_type}"
+  subnet_id                   = "${var.subnet_id}"
+  key_name                    = "${var.ssh_key_name}"
+  security_groups             = "${var.security_groups}"
+  associate_public_ip_address = false
+
+  tags = {
+
+    Name = "kubernetes-worker-${count.index + 1}"
+  }
+}
 
 #
 # Optionally, call a sub-module
--- hashicorp/terraform/blue-module/variables.tf
+++ hashicorp/terraform/blue-module/variables.tf
@@ -1,12 +1,48 @@
-
-#
-# Constrain Terraform version in case version-specific features are used
-#
-terraform {
-  required_version = ">= 0.12"
+variable "masters" {
+  description = "The number of Kuberntes master nodes to be created. For HA, at least 3 should be created."
+  type        = number
+  default     = 1
 }
 
+variable "master_instance_type" {
+  description = "Kubernetes master Amazon instance type."
+  type        = string
+  default     = "t2.small"
+}
 
-#
-# Optionally, call a sub-module
-#
+variable "workers" {
+  description = "The number of Kuberntes worker nodes to be created."
+  type        = number
+  default     = 2
+}
+
+variable "worker_instance_type" {
+  description = "Kubernetes worker Amazon instance type."
+  type        = string
+  default     = "t2.small"
+}
+
+variable "ami_id" {
+  description = "The ID of the AMI to build the nodes from."
+  type        = string
+  default     = "ami-06faba49dddaecfb6"
+}
+
+variable "subnet_id" {
+  description = "The subnet ID."
+  type        = string
+  default     = "subnet-0fe2965e8bf239c09"
+}
+
+variable "ssh_key_name" {
+  description = "The name of an EC2 Key Pair that can be used to SSH to the EC2 Instances in this cluster. Set to an empty string to not associate a Key Pair."
+  type        = string
+  default     = "sbox-ml-kp-01"
+}
+
+variable "security_groups" {
+  description = "The list of security groups"
+  type        = list
+  default     = ["sg-041f9d51704199e98"]
+}
+
