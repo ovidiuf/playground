9d4a18010f11578c459d4246ae3ea7f149681348
Ovidiu Feodorov <ovidiu@feodorov.com>
2020-09-19 21:59:36 -0700

    ClusterIP service

---
+++ kubernetes/services/ClusterIP/undeploy-probe.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+line=$(kubectl get pods --all-namespaces | grep probe)
+[[ -z ${line} ]] && { echo "[warning]: no probe pod found in any namespace"; exit 0; }
+namespace=${line%% *}
+
+kubectl -n ${namespace} delete pod probe --grace-period=0 --force && echo "probe undeployed from namespace ${namespace}"
