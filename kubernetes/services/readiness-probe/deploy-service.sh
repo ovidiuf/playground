cf3224768be8f657b2aadf3b21a4cc1ac10477db
Ovidiu Feodorov <ovidiu@feodorov.com>
2020-09-21 23:25:22 -0700

    readiness probe

---
+++ kubernetes/services/readiness-probe/deploy-service.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+namespace=$1
+[[ -z ${namespace} ]] && { echo "[error]: namespace must be specified" 1>&2; exit 1; }
+kubectl -n ${namespace} apply -f ./service-clusterIP.yaml
+kubectl -n ${namespace} apply -f ./pod-first.yaml
+kubectl -n ${namespace} apply -f ./pod-second.yaml
