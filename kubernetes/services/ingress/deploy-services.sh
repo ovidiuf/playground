567f9c0ce44e29d7ce1f06aeb33bc4f55858df81
Ovidiu Feodorov <ovidiu@feodorov.com>
2020-09-25 18:22:11 -0700

    ingress

---
+++ kubernetes/services/ingress/deploy-services.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+namespace=$1
+[[ -z ${namespace} ]] && namespace=default
+
+kubectl -n ${namespace} apply -f ./service-a.yaml
+kubectl -n ${namespace} apply -f ./service-b.yaml
+kubectl -n ${namespace} apply -f ./pod-service-a.yaml
+kubectl -n ${namespace} apply -f ./pod-service-b.yaml
+
+echo "deployed in namespace ${namespace}"
---
+++ kubernetes/services/ingress/undeploy-services.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+namespace=$(kubectl get pod --all-namespaces -l function=serves-http,affiliation=a -o jsonpath="{.items[*].metadata.namespace}")
+
+kubectl -n ${namespace} delete service a
+kubectl -n ${namespace} delete service b
+kubectl -n ${namespace} delete pod httpd-a --grace-period=0 --force
+kubectl -n ${namespace} delete pod httpd-b --grace-period=0 --force
+
+echo "service and pods undeployed from namespace ${namespace}"
