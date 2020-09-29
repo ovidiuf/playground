607e559f9f07a97c4c998a750a679ad1adb4a7f0
Ovidiu Feodorov <ovidiu@feodorov.com>
2020-09-29 16:24:40 -0700

    gradle

---
+++ gradle/extending-gradle/03-enhanced-task-in-buildSrc/buildSrc/src/main/java/playground/gradle/CustomEnhancedTask.java
@@ -0,0 +1,11 @@
+package playground.gradle;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class CustomEnhancedTask extends DefaultTask {
+   @TaskAction
+   void impl() {
+      System.out.println("this is an custom enhanced task developed in buildSrc");
+   }
+}
