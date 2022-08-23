bf28758f2bc41bdd9aeca536810db6c3e0966343
Ovidiu Feodorov <ovidiu@feodorov.com>
2022-08-22 21:45:11 -0700

    Python experiments

--- pyhton/experiments/main.py
+++ pyhton/experiments/main.py
@@ -1,7 +1,11 @@
 import logging
+from A import A
 
-logging.basicConfig(level=logging.DEBUG)
-some_logger = logging.getLogger('some-logger')
+# logging.basicConfig(level=logging.DEBUG)
+# some_logger = logging.getLogger('some-logger')
+#
+# logging.debug('debugging')
+# some_logger.debug('debugging via some_logger')
 
-logging.debug('debugging')
-some_logger.debug('debugging via some_logger')
+a = A()
+print(a.color)
